6df33762700887624a375b66fa6857a0f369c8f4
ReemSh <reem.shamasnah@gmail.com>
2024-01-03 19:56:02 +0200

    remove extra comments

--- server/routes/api.js
+++ server/routes/api.js
@@ -61,28 +61,3 @@ router.put('/update/:group1/:group2', function(req, res){
 module.exports = router
 
 
-// app.put('/person/:id', function (req, res) {
-//     const personId = req.params.id
-//     Person.findByIdAndUpdate(personId,  { age: 80 }, { new: true }).then( function (person) {
-//         console.log(person)
-//     })
-//     res.end()
-// })
-// app.post('/person', function (req, res) {
-//     const personData = req.body.person
-//     const newPerson = new Person (personData)
-//     newPerson.save() 
-//     res.end()
-// })
-// app.delete('/apocalypse', function (req, res) {
-//     Person.deleteMany().then(function(){
-//         console.log("Data deleted"); // Success
-//     }).catch(function(error){
-//         console.log(error); // Failure
-//     });
-//     Person.find({}).then( function (people) {
-//         console.log(people)
-//     })
-//     res.end()
-// })
-
